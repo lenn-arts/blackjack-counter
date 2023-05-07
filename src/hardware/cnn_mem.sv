1689ecbbbee7283a0f24f88a1ca39ed446eccb8e
Lennart Schulze <schulzelennart@gmail.com>
2023-05-07 13:06:04 -0400

    update hardware for io

--- src/hardware/cnn_mem.sv
+++ src/hardware/cnn_mem.sv
@@ -14,16 +14,16 @@ module cnn_mem(
 		output logic [7:0] val_out); // must be multiple of 8!
 
 	reg [7:0]	value;
-	reg [16:0] params;
-	reg [16:0] img;
-	reg [256:0] ram;
+	reg [15:0] params;
+	reg [15:0] img;
+	reg [255:0] ram;
 
 	localparam bandwidth = 7; // for 8 bit
 
 	always_ff @(posedge clk) begin
 		if (reset) begin
-			ram[7:0] = 8'd0;
-		end else if (chipselect && write)
+			ram[255:0] = 256'd0;
+		end else if (chipselect && write) begin
 			//case (address)
 			//	2'h0 : 
 			//		value <= writedata;
@@ -37,11 +37,14 @@ module cnn_mem(
 			//ram[address+bandwidth:address] <= writedata;
 			ram[address] <= writedata;
 			//endcase
+		end else if (chipselect && read) begin
+			val_out <= ram[address];
+		end
 	end
 
-	always_comb begin
-		val_out = ram[bandwidth:0];
-	end
+	//always_comb begin
+	//	val_out = ram[bandwidth:0];
+	//end
 	       
 endmodule
 
