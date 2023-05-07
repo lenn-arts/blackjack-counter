9be9f8905e03e333cd9dcd3a0f6db7331087cdfa
Lennart Schulze <schulzelennart@gmail.com>
2023-05-07 15:31:51 -0400

    add flag

--- src/software/cnn_io.c
+++ src/software/cnn_io.c
@@ -49,7 +49,7 @@ static void write_value(int val[], int max_addr){
 static int* read_value(int addr, int max_addr){
     /* ioread(adress-to-read-from)*/
     //static int out[max_addr-addr]; // doesnt work because dynamic size and static (needs static to retain mem addr outside the fucntion)
-    int* out_ptr = kmalloc(sizeof(int)*(max_addr-addr)); // dynamic allocation
+    int* out_ptr = kmalloc(sizeof(int)*(max_addr-addr), GFP_KERNEL); // dynamic allocation
     int addr_local;
     for (addr_local = 0; addr_local < max_addr-addr; addr_local = addr_local + 1){
         *(out_ptr+addr_local) = ioread8(dev.virtbase+addr+addr_local);
