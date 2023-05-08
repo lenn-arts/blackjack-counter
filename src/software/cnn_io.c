444b8e6d7dc8bff4e4b3cad54228e1e83e710c4e
Lennart Schulze <schulzelennart@gmail.com>
2023-05-07 20:45:18 -0400

    debug

--- src/software/cnn_io.c
+++ src/software/cnn_io.c
@@ -95,7 +95,7 @@ static long cnn_ioctl(struct file *f, unsigned int cmd, unsigned long val_arg)
             //int *arr_ptr_local;
             int* arr_ptr_local = read_value(0,10);
             pr_info("ictl_reading: done reading %d", arr_ptr_local);
-            pr_info("ictl_reading: val_local[0] %d", *(arr_ptr_local));
+            pr_info("ictl_reading: val_local[0] %d, %d", *(arr_ptr_local), arr_ptr_local[0]);
             pr_info("\n");
             //pr_info("val arg: %d", val_arg)
             // copy from local to arg
