0687e8f719fa7bb406869437a8b2f8eb0864697f
Lennart Schulze <schulzelennart@gmail.com>
2023-05-07 22:08:23 -0400

    fix

--- src/software/test.c
+++ src/software/test.c
@@ -70,8 +70,8 @@ int main()
     int value_local;
     static const char filename[] = "/dev/cnn_mem";
 
-    static int size=130;
-    int img[size] = {0};
+    int size=130;
+    int img[130] = {0};
     int i;  // Loop variable
     for (i = 0; i < size; i=i+1) // Using for loop we are initializing
     {
