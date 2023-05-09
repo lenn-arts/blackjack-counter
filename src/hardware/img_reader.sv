0d90f0b9c2761f3a96ab71c2e9f1b3843bd0652f
Lennart Schulze <schulzelennart@gmail.com>
2023-05-09 19:45:37 -0400

    debug hw

--- src/hardware/img_reader.sv
+++ src/hardware/img_reader.sv
@@ -20,37 +20,38 @@ module img_reader(
 		output logic [31:0] readdata,
 		output logic get_img); // must be multiple of 8!
 
-	assign readdata = get_img ? {VGA_R, VGA_G, VGA_B, 8'd0} : counter_to_zeros;
+	assign readdata = get_img ? {VGA_R, VGA_G, VGA_B, 8'd0} : 32'd9999999;
 
-	logic [31:0] counter_to_zeros = 0;
-	logic [31:0] counter = 0;
+	logic [31:0] counter_to_zeros = 32'd0;
+	logic [31:0] counter = 32'd0;
 
 	always_ff @(posedge clk) begin
 		// RESET
 		if (reset) begin
-			get_img <= 0;
+			get_img <= 1'd0;
 		end else if (chipselect && read) begin
 			case(address)
 				8'b0: begin
-					get_img <= 1;
-					counter_to_zeros <= 0;
-					counter <= counter + 1;
+					get_img <= 1'd1;
+					counter_to_zeros <= 32'd0;
+					counter <= counter + 32'd1;
 					if (!HSYNC && !VSYNC) begin
 						counter_to_zeros <= counter;
+						counter <= 32'd0; // here
 					end
 					break;
 				end
 				8'b1: begin
-					get_img <= 0;
-					counter <= 0;
-					counter_to_zeros <= 0;
+					get_img <= 1'd0;
+					counter <= 32'd0;
+					counter_to_zeros <= 32'd0;
 					break;
 				end
 			endcase
 		end else begin 
-			get_img <= 0;
-			counter <= 0;
-			counter_to_zeros <= 0;
+			get_img <= 1'd0; // here:: 
+			counter <= 32'd0;
+			counter_to_zeros <= 32'd0;
 		end
 	end
 
