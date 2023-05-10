fbff8ca07003e739b39fa9d73a3106bf527ee2df
mjo2156 <100459621+mjo2156@users.noreply.github.com>
2023-05-10 17:15:31 -0400

    Update test.c

--- src/software/test.c
+++ src/software/test.c
@@ -40,22 +40,22 @@ void print_image(int *arr, int numrows, int numcols) {
           lsbyte = (char *) ptr + 0;
           sprintf(buffer, "%d", *lsbyte);
           fputs(buffer, fp);
-          fputs(" ", fp;
+          fputs(" ", fp);
           //printf("Least significant byte is: %s ", buffer);
           rbyte = (char *) ptr + 3;
           sprintf(buffer, "%d", *rbyte);
-          fputs(buffer, fp;
-          fputs(" ", fp;
+          fputs(buffer, fp);
+          fputs(" ", fp);
           //printf("Red byte is: %s ", buffer);
           gbyte = (char *) ptr + 2;
           sprintf(buffer, "%d", *gbyte);
-          fputs(buffer, fp
-          fputs(" ", fp;
+          fputs(buffer, fp);
+          fputs(" ", fp);
           //printf("Green byte is: %s ", buffer);
           bbyte = (char *) ptr + 1;
           sprintf(buffer, "%d", *bbyte);
-          fputs(buffer, fp;
-          fputs("\n", fp;
+          fputs(buffer, fp);
+          fputs("\n", fp);
           //printf("Blue byte is: %s \n", buffer);
       } 
   }
