83d2359b739817a47a7a24539a0431fd9d5b1e3d
Lennart Schulze <schulzelennart@gmail.com>
2023-05-10 02:32:15 -0400

    test

--- src/software/test.c
+++ src/software/test.c
@@ -82,8 +82,8 @@ int main()
     static const char filename_cnn[] = "/dev/cnn_mem";
     static const char filename_img_reader[] = "/dev/img_reader";
 
-    int size=2000;
-    int img[2000] = {0};
+    int size=500;
+    int img[500] = {0};
     int i;  // Loop variable
     /*for (i = 0; i < size; i=i+1) // Using for loop we are initializing
     {
