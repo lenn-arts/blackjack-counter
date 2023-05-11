1c95eae45e3d65089d1d69a0f15eef2d9042819d
Lennart Schulze <schulzelennart@gmail.com>
2023-05-11 01:29:34 -0400

    restore test

--- src/software/test.c
+++ src/software/test.c
@@ -20,7 +20,61 @@
 
 int cnn_fd;
 int img_reader_fd;
+char path_to_img[] = "./rgb888img";
+char path_to_img_bin[] = "./rgb888imgbin";
 
+// as long as numrows and cols is correct, it doesnt matter that last value is offset
+void print_image(int *arr, int numrows, int numcols) {
+  int *ptr;
+  char* rbyte, *gbyte, *bbyte, *lsbyte;
+  FILE* fp, *fpbin;
+  char buffer[4];
+  fp = fopen(path_to_img, "w");
+  if (fp == NULL) {
+      perror("Failed: ");
+      return 1;
+  }
+  fpbin = fopen(path_to_img_bin, "w");
+  if (fpbin == NULL) {
+      perror("Failed: ");
+      return 1;
+  }
+ 
+  for (int i = 0; i < numrows; i++) {
+      for (int j = 0; j < numcols; j++) {
+          //if (i % 12 != 0) continue;
+          ptr = (int *) arr + i*numcols + j;
+          //printf("Image at row %d, col %d:\n", i, j);
+          /*lsbyte = (char *) ptr + 0;
+          sprintf(buffer, "%d", *lsbyte);
+          fputs(buffer, fp);
+          fputs(" ", fp);*/
+          //printf("Least significant byte is: %s ", buffer);
+          rbyte = (char *) ptr + 3;
+          sprintf(buffer, "%d", *rbyte);
+          fputs(buffer, fp);
+          fwrite(rbyte, sizeof(char), 1, fpbin);
+          fputs(" ", fp);
+          //printf("Red byte is: %s ", buffer);
+          gbyte = (char *) ptr + 2;
+          sprintf(buffer, "%d", *gbyte);
+          fputs(buffer, fp);
+          fwrite(gbyte, sizeof(char), 1, fpbin);
+          fputs(" ", fp);
+          //printf("Green byte is: %s ", buffer);
+          bbyte = (char *) ptr + 1;
+          sprintf(buffer, "%d", *bbyte);
+          fputs(buffer, fp);
+          fwrite(bbyte, sizeof(char), 1, fpbin);
+          fputs("\n", fp);
+          //printf("Blue byte is: %s \n", buffer);
+      } 
+  }
+  
+  fclose(fp);
+  fclose(fpbin);
+  
+}
 /* Read and print the background color */
 void print_value(void) {
   int value_local;
@@ -36,10 +90,34 @@ void print_value(void) {
   end:;
 }
 
+// inspired by online resources on array rotating
+void reverse(int arr[], int start, int end)
+{
+  while (start <= end)
+  {
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+}
+// Function to Rotate k elements to right
+void rotatetoright(int arr[], int num_elem, int k)
+{
+  // Reverse first n-k elements
+  reverse(arr, 0, num_elem - k - 1);
+  // Reverse last k elements
+  reverse(arr, num_elem - k, num_elem - 1);
+  // Reverse whole array
+  reverse(arr, 0, num_elem - 1);
+}
+
 /* Set the background color */
 int* get_value(int mode)
 {
-  int* value_local = malloc((640*480+1)*sizeof(int));
+  int* value_local = malloc((640 * 480 + 1) * sizeof(int)); // plus 1 for the offset bit
+  char* rbyte, *gbyte, *bbyte, *lsbyte; // To check that the least significant byte is always 0
   if (mode==0){ // regular mode
     printf("get_val: READ_VAL");
     if (ioctl(cnn_fd, CNN_READ_VAL, value_local)) {
@@ -52,9 +130,24 @@ int* get_value(int mode)
         perror("ioctl(IMG_READ) failed");
         return -1;
     }
+    lsbyte = value_local;
+    int offset = *(value_local+640*480);
+    printf("pointer is %d\n", offset);
+    printf("Least significant byte is: %d\n", *lsbyte);
+    rbyte = (char *) value_local + 3;
+    printf("Red byte is: %d\n", *rbyte);
+    gbyte = (char *) value_local + 2;
+    printf("Green byte is: %d\n", *gbyte);
+    bbyte = (char *) value_local + 1;
+    printf("Blue byte is: %d\n", *bbyte);
+    //printf("%d", value_local);
+
+    //memmove(&lsbyte[k+1], &lsbyte[k], (numItems-k-1)*sizeof(double));
+    //items[k] = value;
+    rotatetoright(lsbyte, 640*480, 500);
   }
   printf("Uget_value: ptr: %d \t ptr[0]: %d\n", value_local, *(value_local));
-  //printf("%d", value_local);
+  
   return value_local;
 };
 
@@ -83,15 +176,9 @@ int main()
     int value_local;
     static const char filename_cnn[] = "/dev/cnn_mem";
     static const char filename_img_reader[] = "/dev/img_reader";
-
-    int numlines = 480;
+    int numlines=480;
     int size=640;
-    //int img[500] = {0};
     int i;  // Loop variable
-    /*for (i = 0; i < size; i=i+1) // Using for loop we are initializing
-    {
-        img[i] = i;
-    }*/
 
     int weights_l1 = {0};
     int weights_l2 = {0};
@@ -109,19 +196,22 @@ int main()
         return -1;
     }
 
-    // printf("initial state: ");
-
-    //for (i = 0 ; i < 24 ; i++) {
-    //long arr_ptr = (long) img; // &arr
-    //printf("\nU arr_ptr: %d", arr_ptr);
-    //set_value(arr_ptr, 0);
-    //int* ptr = get_value();
-    int* ptr = get_value(1);
+    /*int* ptr[numlines];
+    for (i = 0; i < numlines; i++) {
+      printf("Line number %d\n", i);
+      ptr[i] = get_value(1);
+    }
+    for (i = 0; i < numlines; i++) {
+      free(ptr[i]); 
+    }*/
+    //usleep(2000000);
+    int *ptr = get_value(1);
+    print_image(ptr, 480, 640);
     printf("main: got value %d", ptr);
     printf("main: got value %d", ptr[0]);
     free(ptr);
+
     usleep(400000);
-    
     //}
 
     printf("CNN Userspace program terminating\n");
