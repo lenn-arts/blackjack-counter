360ed07bbecefc490566be630b18ac1a8ab9650b
Lennart Schulze <schulzelennart@gmail.com>
2023-05-11 00:22:49 -0400

    delays

--- src/software/img_reader.c
+++ src/software/img_reader.c
@@ -61,7 +61,7 @@ static int* read_img(int max_reads){
     for (i_read = 0; i_read < max_reads; i_read = i_read +1){
         //*(out_ptr+addr_local) = ioread16(dev.virtbase+addr+addr_local);
         *(out_ptr+i_read) = ioread32(dev.virtbase+0); // here; 
-        ndelay(10000);
+        ndelay(30000);
         //pr_info("Kread_value: from %d (%d) read %d (%b)", i_read, dev.virtbase, *(out_ptr+i_read), *(out_ptr+i_read));
     }
     int offset_zero = ioread32(dev.virtbase+4);
