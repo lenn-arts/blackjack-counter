617d1a47596f761b89ff6d7ad1783cd04faf8624
Lennart Schulze <schulzelennart@gmail.com>
2023-05-10 23:39:04 -0400

    debug

--- src/software/img_reader.c
+++ src/software/img_reader.c
@@ -64,7 +64,7 @@ static int* read_img(int max_reads){
         //pr_info("Kread_value: from %d (%d) read %d (%b)", i_read, dev.virtbase, *(out_ptr+i_read), *(out_ptr+i_read));
     }
     int offset_zero = ioread32(dev.virtbase+4);
-    *(out_ptr+i_read+1) = offset_zero;
+    //*(out_ptr+i_read+1) = offset_zero;
     pr_info("Kread_value: offset zero %d", offset_zero);
     pr_info("\n", offset_zero);
     //pr_info("Kread_value: returning %d", out_ptr);
