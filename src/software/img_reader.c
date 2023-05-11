51be847e58724ff7a955d11b16e0c72434ac3bf9
Lennart Schulze <schulzelennart@gmail.com>
2023-05-11 01:42:29 -0400

    remove delay

--- src/software/img_reader.c
+++ src/software/img_reader.c
@@ -63,7 +63,7 @@ static int* read_img(int max_reads){
         *(out_ptr+i_read) = ioread32(dev.virtbase+0); // here; 
         //*(out_ptr+i_read) = ioread32(dev.virtbase+0);
         //usleep(1);
-        ndelay(50000); // 100 000 works
+        //ndelay(50000); // 100 000 works
         //pr_info("Kread_value: from %d (%d) read %d (%b)", i_read, dev.virtbase, *(out_ptr+i_read), *(out_ptr+i_read));
     }
     ndelay(30000);
