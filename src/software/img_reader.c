e3e83eb932ffc49ad6b8ca9c60b689e8d399638f
Lennart Schulze <schulzelennart@gmail.com>
2023-05-11 01:08:48 -0400

    fix

--- src/software/img_reader.c
+++ src/software/img_reader.c
@@ -107,7 +107,7 @@ static long img_reader_ioctl(struct file *f, unsigned int cmd, unsigned long val
             pr_info("ictl_reading: done reading %d", arr_ptr_local);
             pr_info("ictl_reading: val_local[0] %d, %d", *(arr_ptr_local), (int) arr_ptr_local[0]);
             pr_info("\n");
-            if (copy_to_user(arr_ptr, arr_ptr_local, sizeof(int)*size)){
+            if (copy_to_user(arr_ptr, arr_ptr_local, sizeof(val_local))){
                 pr_info("copy to user failed");
                 return -EACCES;
             }
